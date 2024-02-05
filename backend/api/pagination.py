d9168647d716fc746061c103dc4325a5c780dc78
Алина Данкова <alina.emelianova@icloud.com>
2024-02-05 22:01:32 +0400

    Fix pagination

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,7 +1,9 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 
 
-class GeneralPagination(LimitOffsetPagination):
+class GeneralPagination(PageNumberPagination):
     """Общий пагинатор для записей."""
 
-    default_limit = 100
+    page_size = 10
+    page_size_query_param = 'limit'
+    page_query_param = 'page'
